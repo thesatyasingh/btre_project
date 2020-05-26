15e318371728d8b2051518bcb210d2e73fd9538c
Satyapal Singh <satyasingh100@gmail.com>
2020-05-26 16:19:34 +0530

    PROD settings

--- btre/settings.py
+++ btre/settings.py
@@ -145,12 +145,6 @@ MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 }
 
-EMAIL_HOST='smtp.gmail.com'
-EMAIL_PORT=587
-EMAIL_HOST_USER='satyasingh100@gmail.com'
-EMAIL_HOST_PASSWORD='$@Tya054218017'
-EMAIL_USE_TLS=True
-
 try:
     from .local_settings import *
 except ImportError:
