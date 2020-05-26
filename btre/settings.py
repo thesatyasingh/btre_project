7c4d0277cf1abd8488190fcb22ac404ef615ba3e
Satyapal Singh <satyasingh100@gmail.com>
2020-05-26 16:09:34 +0530

    Added local settings

--- btre/settings.py
+++ btre/settings.py
@@ -150,3 +150,8 @@ EMAIL_PORT=587
 EMAIL_HOST_USER='satyasingh100@gmail.com'
 EMAIL_HOST_PASSWORD='$@Tya054218017'
 EMAIL_USE_TLS=True
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
